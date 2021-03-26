5cb5f030831e7a70790d8ceca9008a9cb5417f79
LeBarney <barneywarframe@gmail.com>
2021-03-26 17:40:16 +0100

    tail fini

--- tail.js
+++ tail.js
@@ -1,9 +1,41 @@
 const fs = require('fs')
 
-let texte = fs.readFileSync(process.argv[2], 'utf-8')
+var index = 2;
+var nombredeligne = 10;
 
-const tab1 = texte.split('\n') 
-const rev = tab1.reverse()
-const compte = rev.slice(0,10)
 
-console.log(compte)
+if(process.argv.length != 3 && process.argv.length != 5){
+  console.log("Usage : node tail.js (-n longueur) file.txt")
+  process.exit(1)
+}
+if(process.argv[2] === '-n'){
+  index += 2;
+  nombredeligne = process.argv[3];
+}
+
+if (!fs.existsSync(process.argv[index])) {
+
+  console.log(`Désolé ${process.argv[index]} n\'existe pas`)
+  process.exit(1)
+}
+
+
+  let texte = fs.readFileSync(process.argv[index], 'utf-8')
+
+
+  const tab1 = texte.split('\n') 
+  const rev = tab1.reverse()
+  const compte = rev.slice(0, nombredeligne)
+  const alendroit = compte.reverse()
+  const joinalendroit = alendroit.join('\n')
+  console.log(joinalendroit)
+
+
+
+  
+
+
+
+
+
+
